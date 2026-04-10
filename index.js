2672bace8e4bef617963927d4fa575d034f860ca
Ashish Prajapati <116935348+Anticoder03@users.noreply.github.com>
2026-04-10 21:04:07 +0530

    added indes.js

---
+++ index.js
@@ -0,0 +1,22 @@
+function test() {
+    console.log("Hello, World!");
+}
+
+test()
+
+function test2(params) {
+    console.log(`Hello ,${params}`)
+}
+
+// test2("Raj")
+
+let greet = () =>{
+    console.log("Hello World")
+}
+// setTimeout(greet,5000)
+
+let func = function (){
+    console.log("Hii")
+}
+
+func()
