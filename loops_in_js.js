8435f5001db2cd1efcaa1c00d637a95a6f7af52a
Ashish Prajapati <116935348+Anticoder03@users.noreply.github.com>
2026-04-11 14:29:26 +0530

    added loops

---
+++ loops_in_js.js
@@ -0,0 +1,13 @@
+for (let i=1;i<=10;i++){
+    console.log(`For Loop Itreation: ${i}`);
+}
+let a = 1;
+while (a<=10) {
+    console.log(`While Loop Itreation: ${a}`)
+    a++;
+}
+a = 1
+do{
+    console.log(`Do-While Loop Itreation: ${a}`)
+    a++;
+}while(a<=10);
