f105e6d38a086ac386aa0d10485883a1d760c5f8
Ashish Prajapati <116935348+Anticoder03@users.noreply.github.com>
2026-04-20 17:27:33 +0530

    added sorting arrar and finding max

--- array/array.js
+++ array/array.js
@@ -7,7 +7,7 @@ let sum = 0;
 for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
 }
-console.log(sum)
+// console.log(sum)
 
 //? using forEach
 
@@ -15,9 +15,33 @@ sum = 0;
 arr.forEach(element => {
     sum += element;
 });
-console.log(sum)
+// console.log(sum)
 
 //? other approch
 
 let sum1 = arr.reduce((acc, curr) => acc + curr, 0);
-console.log(sum1)
+// console.log(sum1)
+
+
+// find maxmimum number form array
+let max = arr[0];
+
+for (let i = 1; i < arr.length; i++) {  
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+}
+// console.log(max)
+
+//? using reduce
+
+let max1 = arr.reduce((acc, curr) => acc > curr ? acc : curr, arr[0]);
+// console.log(max1)
+
+//? SORTED function
+arr2 = arr.sort((a, b) => a - b);
+// console.log(arr2[arr2.length - 1])
+
+
+// ? max function
+// console.log(Math.max(...arr))
