25a15e76cd0806df6931529f386b6dfa190f935a
Ashish Prajapati <116935348+Anticoder03@users.noreply.github.com>
2026-04-20 18:51:09 +0530

    added more test

--- array/array.js
+++ array/array.js
@@ -41,7 +41,46 @@ let max1 = arr.reduce((acc, curr) => acc > curr ? acc : curr, arr[0]);
 //? SORTED function
 arr2 = arr.sort((a, b) => a - b);
 // console.log(arr2[arr2.length - 1])
+// console.log(arr2[0])
 
 
 // ? max function
 // console.log(Math.max(...arr))
+// console.log(Math.min(...arr))
+
+let students = [
+    { name: "A", marks: 20 },
+    { name: "B", marks: 22 },
+    { name: "C", marks: 19 },
+    {name: "D", marks: 25},
+];
+
+students.sort((a, b) => b.marks - a.marks);
+// console.log(students)    
+// console.log(students.reverse())
+
+// REMOVE DUPLICATE ELEMENTS FROM ARRAY
+let arr3 = [1, 2, 3, 4, 5, 1, 2, 3];
+
+let uniqueArr = [...new Set(arr3)];
+// console.log(uniqueArr)
+
+let repeted_arr = [1,2,3,4,5,1,2,3];
+
+let unique_arr = [...new Set(repeted_arr)];
+// console.log(unique_arr)
+
+// console.log(uniqueArr[uniqueArr.length - 2])
+// console.log(uniqueArr[1])
+
+
+arr3 = [1, 2, 3, 4, 5, 1, 2, 3];
+let unique = [];
+
+for (let i = 0; i < arr3.length; i++) { 
+    if (!unique.includes(arr3[i])) {
+        unique.push(arr3[i]);
+    }   
+}
+console.log(unique)
+
